09151c3b4f339f87ad258836af430c323bfdba0e
Saumya-Mishra9129 <saumyamishra9129853131@gmail.com>
2020-08-19 20:53:09 +0530

    Fix pygame.mixer initialization

--- activity.py
+++ activity.py
@@ -21,7 +21,7 @@ class Activity(activity.Activity):
         self.max_participants = 1
         self.sound = True
         self.game = main.game()
-        self.game.canvas = sugargame.canvas.PygameCanvas(self, main=self.game.make, modules=[pygame.display, pygame.font])
+        self.game.canvas = sugargame.canvas.PygameCanvas(self, main=self.game.make, modules=[pygame.display, pygame.font, pygame.mixer])
         self.set_canvas(self.game.canvas)
         self.game.canvas.grab_focus()        
         self.build_toolbar()
--- main.py
+++ main.py
@@ -41,15 +41,8 @@ from score_path import score_path
 class game:
 
     def make(self):
-
-        gameDisplay = display_init()
         sound = True
-
-        try:
-            pygame.mixer.init()
-        except Exception as err:
-            sound = False
-            print('error with sound', err)
+        gameDisplay = display_init()
 
         black = (0, 0, 0)
         white = (255, 255, 255)
